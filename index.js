e05b24d46dc3e953cea5566b350c9760a765a6e4
Geoffrey Liu <g-liu@users.noreply.github.com>
2022-10-18 21:24:20 -0700

    CSRF failing hard :(

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ console.log("Scraping the organ database...");
 request("https://pipeorgandatabase.org/organs/search/quick", function(err, res, body) {
   // need #csrf_token value
   if (err || res.statusCode != 200) {
-    console.log("an error was encountered");
-    console.log(err);
+    console.error("an error was encountered");
+    console.error(err);
     return;
   }
   console.log("data received for search page");
@@ -19,21 +19,39 @@ request("https://pipeorgandatabase.org/organs/search/quick", function(err, res, 
   var csrf = $("#csrf_token").val();
   console.log(`Got csrf token ${csrf}`);
 
-  request(`https://pipeorgandatabase.org/organs/search/quick?csrf_token=${csrf}&city=seattle`, function(err, res, body) {
+  // TODO: MAKE CONFIGURABLE
+  var city = "Seattle";
+
+  request(`https://pipeorgandatabase.org/organs/search/quick?csrf_token=${csrf}&institution=&city=${city}&builder=&opus=&year=&state=&country=`, function(err, res, body) {
     if (err || res.statusCode != 200) {
-      console.log("Welp couldn't load results :(");
-      console.log(err);
+      console.error("Welp couldn't load results :(");
+      console.error(err);
       return;
     }
-    console.log("data received for results page");
 
     var $ = cheerio.load(body);
+
+    if ($("#city").length > 0) {
+      // Fuck, we're still on the search page
+      console.error("Fuck, CSRF failed.");
+      return;
+    }
+
+    if ($(".row").text().indexOf("There are no results") != -1) {
+      // Fuck, no results
+      console.log("No results :(");
+      return;
+    }
+
+    console.log("data received for results page");
 
     var allResultsOnPage = $("a[href^='/organ'] > .card");
     var numResults = allResultsOnPage.length;
 
     console.log(`Got ${numResults} results :)`);
     var resultText = allResultsOnPage.text();
+
+    // todo: handle pagination
 
     console.log(resultText);
   });
