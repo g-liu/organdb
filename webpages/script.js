f184bc8e16219a2456a9224b18e607d01a52e9ba
Geoffrey Liu <g-liu@users.noreply.github.com>
2022-10-20 12:32:56 -0700

    IT WORKS and that's all that matters

--- webpages/script.js
+++ webpages/script.js
@@ -5,7 +5,8 @@
   map.addLayer(new OpenLayers.Layer.OSM());
 
   // TODO: READ FROM LOCATION DATA
-  var startingLoc = new OpenLayers.LonLat( 47.5 ,-122.1 )
+  // And why the fuck would you do LON LAT not LAT LONG???????????
+  var startingLoc = new OpenLayers.LonLat( -122.1 , 47.5 )
         .transform(
           new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
           map.getProjectionObject() // to Spherical Mercator Projection
@@ -19,7 +20,7 @@
   locationdata = await (await fetch('./locationdata.json')).json();
   locationdata.forEach(element => {
     if (element == null || element == undefined) { return; }
-    var lonLat = new OpenLayers.LonLat(element.lat, element.lon)
+    var lonLat = new OpenLayers.LonLat(element.lon, element.lat)
       .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
     
     markers.addMarker(new OpenLayers.Marker(lonLat)); // TODO: Title, location?
