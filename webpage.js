5feedece045fcf20bb2ac056465a09589e72bf3e
Geoffrey Liu <g-liu@users.noreply.github.com>
2022-10-20 11:46:00 -0700

    split up assets.

--- webpage.js
+++ webpage.js
@@ -4,9 +4,15 @@ const open = require('open');
 const port = 8000;
 
 var app = express();
-app.get('/', function(req, res) {
-  res.sendFile('webpages/index.html', { root: __dirname });
-});
+// app.get('/', function(req, res) {
+//   res.sendFile('index.html', { root: __dirname + '/webpages' });
+// });
+
+// app.listen(port);
+
+// open(`http://localhost:${port}`);
+
+app.use(express.static('webpages'));
 
 app.listen(port);
 
